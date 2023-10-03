fb19c6121b78d1e137a7a995b5f2a03eef2a3238
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-10-02 23:13:08 -0400

    Update ece0301_ICA06_step14_Neq4.cpp
    forgot to update this ig

--- ece0301_ICA06_step14_Neq4.cpp
+++ ece0301_ICA06_step14_Neq4.cpp
@@ -62,12 +62,12 @@ int main(){
         }
         output << "]";
 
-        if(i == 0){
+        if(i == ceil(DIM/2) - 1){
             output << " * ";
         } else { output << "   ";}
         output << "[\t" << det[i] << "\t]";
 
-        if(i == 0){
+        if(i == ceil(DIM/2) - 1){
             output << " = ";
         } else { output << "   ";}
         output << "[\t";
